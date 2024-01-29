a17ae6ded1e44d9bf8f201a595b15cea7ffd68c9
Bryan Fischer <fischer.bryan.j@gmail.com>
2024-01-28 21:46:25 -0800

    updated comments

--- LinkedList.h
+++ LinkedList.h
@@ -7,13 +7,30 @@
 template<typename T>
 class LinkedList {
 public:
+
+    // default constructor
+    // set head to nullptr
     LinkedList();
+
+    // destructor
+    // delete all nodes in the list from memory
     ~LinkedList();
 
+    // adds a new node with data value at beginning of list
     void insert(const T& data);
+
+    // finds and removes the first occurance of data value from list
     void remove(const T& data);
+
+    // searches the list for the data value
+    // if found, search returns true, else returns false
     bool search(const T& data) const;
+
+    // prints the list to the terminal
     void display() const;
+
+    // checks if list is empty
+    // returns true if list is empty, else false
     bool isEmpty() const;
 
 private:
--- Node.h
+++ Node.h
@@ -4,9 +4,19 @@
 template<typename T>
 class Node {
 public:
+
+    // EVC Constructor
+    // Sets the Node's data to data from param
+    // Sets next node to nullptr
     Node(const T& data);
+
+    // returns the current value of the node
     T getData() const;
+
+    // returns the next node in the list
     Node<T>* getNext() const;
+
+    // sets the next node in the list to next from param
     void setNext(Node<T>* next);
 
 private:
